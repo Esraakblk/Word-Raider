b86cd4af44d8ec2bfdc860a13671832fdfd581dc
esraakblk <esraakbulak@gmail.com>
2024-10-03 22:28:38 +0300

    Game variables defined and player commands printed

--- basics.py
+++ basics.py
@@ -8,3 +8,15 @@ with open("words.txt", "r") as file:
         word_list.append(word) # Add the word to the word list
 
 guess_the_word = random.choice(word_list)
+
+# Variables to keep track of game information
+misplaced_guesses = []
+incorrect_guesses = []
+max_turns = 5
+turns_taken = 0
+
+#Player redirects
+print(f"Welcome to {game_title}")
+print(f"The word you have to guess consists of {len(guess_the_word)} letters.")
+print(f"You have {max_turns} rounds to guess the word")
+
